3faf11ce22218c42bebeb31b294bf74612ac15f3
James <jamsie72@gmail.com>
2026-01-24 17:24:59 -0500

    no polling on Serial, use blocking reads now. blink the LED to indicate we're alive.

---
+++ esp32io.h
@@ -0,0 +1,5 @@
+/* This structure holds all global and shared runtime info */
+
+struct runtime_data {
+
+} ; typedef struct runtime_data S_RuntimeData ;
--- esp32io.ino
+++ esp32io.ino
@@ -1,65 +1,76 @@
-#define DEF_SERIAL_BAUD	115200
-#define DEF_RGBLED_PIN 48
-#define DEF_THREAD_STACKSIZE 2048
-#define DEF_CONSOLE_THREAD_DELAY_MS 20
-#define DEF_CONSOLE_THREAD_PRIORITY 1
-#define BUF_LEN_CONSOLE 256
+/*
+   Overview
+
+   This program manages the operations on an ESP32. Commands can be sent to
+   the device via serial console or via a web server over wifi. All commands
+   are sent to a queue.
+
+*/
+
+#define DEF_SERIAL_BAUD	115200          // serial port (over USB)
+#define DEF_RGBLED_PIN 48               // RGB led on ESP32-S3 dev board
+#define DEF_RGBLED_BLINK_MS 10          // how long LED stays on
+#define DEF_THREAD_STACKSIZE 2048       // stack size when thread is created
+#define DEF_CONSOLE_THREAD_PRIORITY 1   // thread scheduling priority
+
+#define BUF_LEN_CONSOLE 256             // user command buffer on serial
 
 #include <WiFi.h>
+
+#include "esp32io.h"
+
+// Global Variables
+
+S_RuntimeData G_runtime ;
 
 // ============================================================================
 
 void f_serial_console_thread(void *param)
 {
   int buf_pos = 0 ;
-  char *buf = NULL ;
+  char *buf = NULL, c ;
 
   buf = (char*) malloc(BUF_LEN_CONSOLE) ;
   while (1)
   {
-    // sit here until we read a full command
+    // sit here and loop until we read a full command
 
     buf_pos = 0 ;
     buf[0] = 0 ;
     Serial.printf("> ") ;
     while (1)
     {
-      if (Serial.available() > 0)
+      Serial.readBytes(&c, 1) ;                 // Block until a char arrives
+      if ((c == 8) || (c == 127))               // handle BS or DEL
       {
-        char c = (char) Serial.read() ;
-        if ((c == 8) || (c == 127))                     // handle BS or DEL
+        if (buf_pos > 0) // do we need to erase a char ?
         {
-          if (buf_pos > 0) // do we need to erase a char ?
-          {
-            Serial.printf("\b \b") ;
-            buf_pos-- ;
-            buf[buf_pos] = 0 ;
-          }
-        }
-        else
-        if ((c == '\n') || (c == '\r') || (buf_pos == BUF_LEN_CONSOLE - 1))
-        {
-          Serial.printf("\r\n") ;
-          if (buf_pos == BUF_LEN_CONSOLE - 1)
-          {
-            Serial.printf("WARNING: Command exceeds %d bytes, ignoring.\r\n",
-                          BUF_LEN_CONSOLE - 1) ;
-            buf[0] = 0 ;
-            buf_pos = 0 ;
-          }
-          break ;
-        }
-        else
-        if (c > 0)                                      // a normal char
-        {
-          Serial.printf("%c", c) ;
-          buf[buf_pos] = c ;
-          buf[buf_pos+1] = 0 ;
-          buf_pos++ ;
+          Serial.printf("\b \b") ;
+          buf_pos-- ;
+          buf[buf_pos] = 0 ;
         }
       }
       else
-        vTaskDelay(DEF_CONSOLE_THREAD_DELAY_MS) ; // resets the task watchdog.
+      if ((c == '\n') || (c == '\r') || (buf_pos == BUF_LEN_CONSOLE - 1))
+      {
+        Serial.printf("\r\n") ;
+        if (buf_pos == BUF_LEN_CONSOLE - 1)
+        {
+          Serial.printf("WARNING: Command exceeds %d bytes, ignoring.\r\n",
+                        BUF_LEN_CONSOLE - 1) ;
+          buf[0] = 0 ;
+          buf_pos = 0 ;
+        }
+        break ;
+      }
+      else
+      if (c > 0)                                // a normal char
+      {
+        Serial.printf("%c", c) ;
+        buf[buf_pos] = c ;
+        buf[buf_pos+1] = 0 ;
+        buf_pos++ ;
+      }
     }
 
     // now handle the command
@@ -75,6 +86,10 @@ void f_serial_console_thread(void *param)
 
 void setup ()
 {
+  // initalize data structures
+
+  memset(&G_runtime, 0, sizeof(G_runtime)) ;
+
   // print out some info to show that we're alive.
 
   delay (1000) ;
@@ -109,6 +124,12 @@ void setup ()
 
 void loop ()
 {
-  delay (1000) ;
+  delay (10000) ;
+
+  // blink the LED to indicate we're alive.
+
+  neopixelWrite(DEF_RGBLED_PIN, 0, 0, 255) ;
+  delay (DEF_RGBLED_BLINK_MS) ;
+  neopixelWrite(DEF_RGBLED_PIN, 0, 0, 0) ;
 }
 
