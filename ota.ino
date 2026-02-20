e6649ce9ee7316b2c684439d19774cc087d5759e
James Tay <james@2longbeans.net>
2026-02-19 23:16:16 -0500

    ota code is able to perform HTTP GET and check content-type/content-length.

--- ota.ino
+++ ota.ino
@@ -1,12 +1,119 @@
+#define OTA_PROTO "http://"             // the only protocol we support
+#define OTA_PORT 80                     // the HTTP port we support
+#define OTA_POLL_MSEC 50                // how often we poll for response
+#define OTA_TIMEOUT_SECS 10             // how long we'll wait for a response
+#define OTA_CONTENT_TYPE "application/octet-stream"     // ie, binary data
+
 /*
-   This function is called from "f_action()". Our job is to perform a software
-   update by downloading from the supplied URL.
+   This function is called from "f_ota_cmd()". Our job is to connect to "host"
+   and pull our firmware image at "uri". The outcome is recorded in the worker
+   thread at "idx".
+*/
+
+void f_ota_http_get(int idx, char *host, char *uri)
+{
+  char buf[BUF_LEN_WEBCLIENT] ;
+  WiFiClient client ;
+
+  // now try perform the HTTP GET using "WiFiClient" for the TCP connection
+
+  if (client.connect(host, OTA_PORT) == false)
+  {
+    snprintf(G_runtime->worker[idx].result_msg, BUF_LEN_WORKER_RESULT,
+             "Cannot connect to '%s' on port %d.\r\n", host, OTA_PORT) ;
+    G_runtime->worker[idx].result_code = 500 ;
+    return ;
+  }
+  snprintf(buf, BUF_LEN_WEBCLIENT,
+           "GET /%s HTTP/1.1\n"
+           "Host: %s\n"
+           "Cache-Control: no-cache\n"
+           "Connection: close\n\n",
+           uri, host) ;
+  client.print(buf) ;
+
+  // sit here polling until we get a response from the webserver
+
+  unsigned long now = millis() ;
+  unsigned long cut_off = now + (OTA_TIMEOUT_SECS * 1000) ;
+  while ((client.available() == 0) && (now < cut_off))
+  {
+    delay(OTA_POLL_MSEC) ;
+    now = millis() ;
+  }
+  if (now >= cut_off)
+  {
+    strncpy(G_runtime->worker[idx].result_msg, "No response from server.\r\n",
+            BUF_LEN_WORKER_RESULT) ;
+    G_runtime->worker[idx].result_code = 500 ;
+    client.stop() ;
+    return ;
+  }
+
+  // data is arriving, read the HTTP response header line by line
+
+  int content_length=0 ;
+  while (client.available() > 0)
+  {
+    String s = client.readStringUntil('\n') ;
+    s.trim() ;
+    if (s.length() == 0)                        // end of HTTP headers
+      break ;
+
+    if ((s.startsWith("HTTP/1.1")) && (s.indexOf("200") < 0))
+    {
+      snprintf(G_runtime->worker[idx].result_msg, BUF_LEN_WORKER_RESULT,
+               "Received - %s\r\n", s.c_str()) ;
+      G_runtime->worker[idx].result_code = 500 ;
+      client.stop() ;
+      return ;
+    }
+    if (s.startsWith("Content-Type:"))
+    {
+      if (G_runtime->config.debug)
+        Serial.printf("DEBUG: f_ota_http_get(): type - %s\r\n", s.c_str()) ;
+
+      char *p = strstr(s.c_str(), " ") ;
+      if (strcmp(p+1, OTA_CONTENT_TYPE) != 0)
+      {
+        snprintf(G_runtime->worker[idx].result_msg, BUF_LEN_WORKER_RESULT,
+                 "Invalid type - %s\r\n", s.c_str()) ;
+        G_runtime->worker[idx].result_code = 500 ;
+        client.stop() ;
+        return ;
+      }
+    }
+    if (s.startsWith("Content-Length: "))
+    {
+      if (G_runtime->config.debug)
+        Serial.printf("DEBUG: f_ota_http_get(): length - %s\r\n", s.c_str()) ;
+
+      char *p = strstr(s.c_str(), " ") ;
+      content_length = atoi(p + 1) ;
+      if (content_length < 1)
+      {
+        snprintf(G_runtime->worker[idx].result_msg, BUF_LEN_WORKER_RESULT,
+                 "Invalid length - %s\r\n", s.c_str()) ;
+        G_runtime->worker[idx].result_code = 500 ;
+        client.stop() ;
+        return ;
+      }
+    }
+  }
+
+
+
+}
+
+
+/*
+   This function is called from "f_action()". Our job is to parse and check
+   the URL supplied by the user. If this passes some basic checks, we'll call
+   "f_ota_http_get()" to proceed with the task.
 */
 
 void f_ota_cmd(int idx)
 {
-  #define OTA_PROTO "http://"
-
   // parse the URL into parts, in particular protocol/host/uri.
 
   char *tokens[2], *cmd=NULL, *url=NULL ;
@@ -41,5 +148,5 @@ void f_ota_cmd(int idx)
   if (G_runtime->config.debug)
     Serial.printf("DEBUG: f_ota_cmd() host(%s) uri(%s)\r\n", host, uri) ;
 
-
+  f_ota_http_get(idx, host, uri) ;
 }
