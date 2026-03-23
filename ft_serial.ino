b45b88e2ba3987b736a01d40c9fe2efd602b24de
James Tay <james@2longbeans.net>
2026-03-23 09:51:45 -0400

    "ft_serial()" now prepares "listen_sd"

--- ft_serial.ino
+++ ft_serial.ino
@@ -21,7 +21,66 @@
 
 void ft_serial(S_UserThread *self)
 {
+  if (self->num_args != 4)
+  {
+    strncpy(self->status, "Incorrect arguments", BUF_LEN_UTHREAD_STATUS) ;
+    self->state = UTHREAD_STOPPED ;
+    return ;
+  }
+
+  int port = atoi(self->in_args[0]) ;
+  int baud = atoi(self->in_args[1]) ;
+  int rx_pin = atoi(self->in_args[2]) ;
+  int tx_pin = atoi(self->in_args[3]) ;
+
+  // setup the listening socket
+
+  int listen_sd = socket(AF_INET, SOCK_STREAM, IPPROTO_IP) ;
+  if (listen_sd < 0)
+  {
+    strncpy(self->status, "Cannot create listen_sd", BUF_LEN_UTHREAD_STATUS) ;
+    self->state = UTHREAD_STOPPED ;
+    return ;
+  }
+  struct sockaddr_in addr ;
+  memset(&addr, 0, sizeof(addr)) ;
+  addr.sin_family = AF_INET ;
+  addr.sin_addr.s_addr = INADDR_ANY ;
+  addr.sin_port = htons(port) ;
+  if (bind(listen_sd, (const struct sockaddr*) &addr, sizeof(addr)) < 0)
+  {
+    close(listen_sd) ;
+    strncpy(self->status, "Cannot bind listen_sd", BUF_LEN_UTHREAD_STATUS) ;
+    self->state = UTHREAD_STOPPED ;
+    return ;
+  }
+  if (listen(listen_sd, 1) != 0)
+  {
+    close(listen_sd) ;
+    strncpy(self->status, "Cannot listen on sd", BUF_LEN_UTHREAD_STATUS) ;
+    self->state = UTHREAD_STOPPED ;
+    return ;
+  }
+
+  // acquire UART lock
+
+  if (xSemaphoreTake(G_runtime->L_uart, 0) != pdTRUE)
+  {
+    close(listen_sd) ;
+    strncpy(self->status, "Cannot lock UART", BUF_LEN_UTHREAD_STATUS) ;
+    self->state = UTHREAD_STOPPED ;
+    return ;
+  }
 
 
 
+
+  // in our main loop, make sure we never block for more than half of
+  // DEF_MAX_THREAD_WRAPUP_MSEC, so that we can perform an orderly exit
+
+
+
+
+  xSemaphoreGive(G_runtime->L_uart) ;
+  close(listen_sd) ;
 }
