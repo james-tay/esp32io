a8dcce3761c4b64d6f17a19980287ad54964a3ad
James Tay <james@2longbeans.net>
2026-03-28 20:25:55 -0400

    "uart_fd" doesn't work out, reading garbage on the serial port

--- ft_serial.ino
+++ ft_serial.ino
@@ -5,7 +5,7 @@
    multiplex between "listen_sd", "client_sd" and the UART
 */
 
-void f_serial_io(S_UserThread *self, int listen_sd)
+void f_serial_io(S_UserThread *self, int listen_sd, int uart_fd)
 {
   int num_fds, result, client_sd=-1 ;
   long long loops=0 ;
@@ -20,16 +20,17 @@ void f_serial_io(S_UserThread *self, int listen_sd)
     tv.tv_sec = 0 ;
     tv.tv_usec = DEF_MAX_THREAD_WRAPUP_MSEC * 1000 / 4 ;
     FD_ZERO(&rfds) ;
+    FD_SET(uart_fd, &rfds) ;            // always monitor "uart_fd"
     FD_SET(listen_sd, &rfds) ;          // always monitor "listen_sd"
-    num_fds = listen_sd + 1 ;
+    num_fds = max(uart_fd, listen_sd) ;
+
     if (client_sd >= 0)                 // monitor "client_sd" if connected
     {
       FD_SET(client_sd, &rfds) ;
-      if (client_sd > listen_sd)
-        num_fds = client_sd + 1 ;
+      num_fds = max(num_fds, client_sd) ;
     }
 
-    result = select(num_fds, &rfds, NULL, NULL, &tv) ;
+    result = select(num_fds+1, &rfds, NULL, NULL, &tv) ;
     if (result)
     {
       if (FD_ISSET(listen_sd, &rfds))   // incoming TCP Client
@@ -40,18 +41,36 @@ void f_serial_io(S_UserThread *self, int listen_sd)
         else
           client_sd = new_sd ;          // really accept new client
       }
-
-
+      if (FD_ISSET(uart_fd, &rfds))     // incoming serial data
+      {
+        int amt = read(uart_fd, iobuf, BUF_LEN_LINE) ;
+        if ((amt > 0) && (client_sd >= 0) &&
+            (write(client_sd, iobuf, amt) != amt))
+        {
+          close(client_sd) ;            // TCP client disconnected ?
+          client_sd = -1 ;
+        }
+      }
+      if ((client_sd >=0 && (FD_ISSET(client_sd, &rfds))) // TCP client input
+      {
+        int amt = read(client_sd, iobuf, BUF_LEN_LINE) ;
+        if (amt < 1)
+        {
+          close(client_sd) ;            // TCP client disconnected ?
+          client_sd = -1 ;
+        }
+        else
+          write(uart_fd, iobuf, amt) ;
+      }
     }
-
-    // now check if data arrived on the serial port
-
-
 
     loops++ ;
     snprintf(self->status, BUF_LEN_UTHREAD_STATUS, "busy:%d loops:%lld",
              result, loops) ;
   }
+
+  if (client_sd >= 0)
+    close(client_sd) ;
 }
 
 /*
@@ -116,7 +135,7 @@ void ft_serial(S_UserThread *self)
     return ;
   }
 
-  // acquire UART lock
+  // acquire UART lock, as there's only 1x hardware UART for user applications
 
   if (xSemaphoreTake(G_runtime->L_uart, 0) != pdTRUE)
   {
@@ -128,10 +147,6 @@ void ft_serial(S_UserThread *self)
 
   // Initialize the serial port and then hand work over to "f_serial_io()"
 
-  #define FT_SERIAL_RX_BUF_SIZE 1024
-  #define FT_SERIAL_TX_BUF_SIZE 0       // 0 means don't use TX ring buffer
-  #define FT_SERIAL_QUEUE_SIZE 0        // 0 means don't want an event queue
-
   self->state = UTHREAD_RUNNING ;
   uart_config_t uart_config = {
       .baud_rate = baud,
@@ -142,27 +157,41 @@ void ft_serial(S_UserThread *self)
       .source_clk = UART_SCLK_DEFAULT
     } ;
   uart_param_config(UART_NUM_2, &uart_config) ;
-  uart_driver_install(UART_NUM_2,
-                      FT_SERIAL_RX_BUF_SIZE,
-                      FT_SERIAL_TX_BUF_SIZE,
-                      FT_SERIAL_QUEUE_SIZE,
-                      NULL,                     // uart queue (ie, no queue)
-                      0) ;                      // interrupt flags (ie, none)
-  uart_set_pin(UART_NUM_2, tx_pin, rx_pin,
-               UART_PIN_NO_CHANGE,              // RTS pin (ie, not used)
-               UART_PIN_NO_CHANGE) ;            // CTS pin (ie, not used)
-  esp_vfs_dev_uart_register() ;                 // exposes "/dev/uart/2"
-
-  // fire off the function which handles all the TCP/serial IO
-
-  f_serial_io(self, listen_sd) ;
+  if (uart_driver_install(UART_NUM_2,
+                          1024,                 // serial RX buffer size
+                          1024,                 // serial TX buffer size
+                          0,                    // queue size (ie, no queue)
+                          NULL,                 // uart queue (ie, no queue)
+                          0) != ESP_OK)         // interrupt flags (ie, none)
+    strncpy(self->status, "uart_driver_install() failed",
+            BUF_LEN_UTHREAD_STATUS) ;
+  else
+  {
+    if (uart_set_pin(UART_NUM_2, tx_pin, rx_pin,
+                     UART_PIN_NO_CHANGE,                // RTS pin (ie, unused)
+                     UART_PIN_NO_CHANGE) != ESP_OK)     // CTS pin (ie, unused)
+      strncpy(self->status, "uart_set_pin() failed", BUF_LEN_UTHREAD_STATUS) ;
+    else
+    {
+      esp_vfs_dev_uart_register() ;                     // exposes "/dev/uart/2"
+      int uart_fd = open("/dev/uart/2", O_RDWR | O_NONBLOCK) ;
+      if (uart_fd < 0)
+        strncpy(self->status, "cannot open /dev/uart/2",
+                BUF_LEN_UTHREAD_STATUS) ;
+      else
+      {
+        f_serial_io(self, listen_sd, uart_fd) ;         // serial/tcp main loop
+        close(uart_fd) ;
+      }
+    }
+  }
 
   // release resources associated with the UART before releasing the lock
 
-  uart_wait_tx_done(UART_NUM_2, pdMS_TO_TICKS(100)) ;
+  uart_wait_tx_done(UART_NUM_2, pdMS_TO_TICKS(100)) ; // ie, 100ms
   uart_driver_delete(UART_NUM_2) ;
-
   xSemaphoreGive(G_runtime->L_uart) ;
   close(listen_sd) ;
   self->state = UTHREAD_STOPPED ;
 }
+
